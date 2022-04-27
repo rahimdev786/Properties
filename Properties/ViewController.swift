2275933adc4bda397823213c03d8d85f0a8a5654
rahim <rahimdev786@gmail.com>
2022-04-27 13:05:29 +0530

    Initial Commit

---
+++ Properties/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Properties
+//
+//  Created by arshad on 4/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
